cb3d3ad12609dbf13605a49957ab37d398f35e53
Papajohn77 <t8190314@aueb.gr>
2024-05-07 16:13:17 +0300

    Chapter 9: Radio buttons, Chips, FAB, Toasts & Snackbars

--- BitsandPizzas/app/src/main/java/com/example/bitsandpizzas/OrderFragment.kt
+++ BitsandPizzas/app/src/main/java/com/example/bitsandpizzas/OrderFragment.kt
@@ -5,8 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.chip.Chip
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 
 class OrderFragment : Fragment() {
 
@@ -20,6 +25,26 @@ class OrderFragment : Fragment() {
 
         val toolbar = view.findViewById<MaterialToolbar>(R.id.toolbar)
         (activity as AppCompatActivity).setSupportActionBar(toolbar)
+
+        val fab = view.findViewById<FloatingActionButton>(R.id.fab)
+        fab.setOnClickListener {
+            val pizzaGroup = view.findViewById<RadioGroup>(R.id.pizza_type_group)
+            val pizzaType = pizzaGroup.checkedRadioButtonId
+            if (pizzaType == -1) {
+                val message = "You need to choose a pizza type"
+                Toast.makeText(activity, message, Toast.LENGTH_LONG).show()
+            } else {
+                var message = when (pizzaType) {
+                    R.id.radio_diavolo -> "Diavolo pizza"
+                    else -> "Funghi pizza"
+                }
+                val parmesanChip = view.findViewById<Chip>(R.id.chip_parmesan)
+                message += if (parmesanChip.isChecked) ", extra parmesan" else ""
+                val chiliOilChip = view.findViewById<Chip>(R.id.chip_chili_oil)
+                message += if (chiliOilChip.isChecked) ", extra chili oil" else ""
+                Snackbar.make(fab, message, Snackbar.LENGTH_LONG).show()
+            }
+        }
 
         return view
     }
